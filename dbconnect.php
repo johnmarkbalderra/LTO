de199709bca88601b9cd648111e4a10b3f0b8e98
Markbaldera <58714695+johnmarkbalderra@users.noreply.github.com>
2024-11-25 11:50:58 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -15,7 +15,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:johnadmin.database.windows.net,1433; Database = land", "CloudSAc76ee709", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:johnadmin.database.windows.net,1433; Database = land", "CloudSAc76ee709", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
