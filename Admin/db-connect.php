91528b44821f999263c9a5b3900f252a93d97655
Markbaldera <58714695+johnmarkbalderra@users.noreply.github.com>
2024-11-25 11:55:55 +0800

    Update db-connect.php

--- Admin/db-connect.php
+++ Admin/db-connect.php
@@ -23,7 +23,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAc76ee709", "pwd" => "{your_password_here}", "Database" => "land", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "CloudSAc76ee709", "pwd" => "", "Database" => "land", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:johnadmin.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
