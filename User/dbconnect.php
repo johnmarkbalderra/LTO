1f5ae725172951946d4ce11a2166d8c28dcbaf72
Markbaldera <58714695+johnmarkbalderra@users.noreply.github.com>
2024-11-25 11:41:45 +0800

    Update dbconnect.php

--- User/dbconnect.php
+++ User/dbconnect.php
@@ -1,15 +1,31 @@
+// <?php
+// // Replace these values with your actual database credentials
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "land";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// ?>
 <?php
-// Replace these values with your actual database credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "land";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:johnadmin.database.windows.net,1433; Database = land", "CloudSAc76ee709", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSAc76ee709", "pwd" => "{your_password_here}", "Database" => "land", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:johnadmin.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
